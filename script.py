76f6b76a80fd4a4198fb9bfaebfd8b3d4086b6fd
974Rider <massonalexandre8@gmail.com>
2025-05-28 12:19:59 +0400

    finalisation du script

--- script.py
+++ script.py
@@ -1,53 +1,84 @@
-import numpy as np
+#importations bibliothèques et définition de la position initiale
+
+import pandas as pd
 import matplotlib.pyplot as plt
-
-# Temps total jusqu'à l'arrêt complet
-t = np.linspace(0, 18, 1000)
-
-# Définition des valeurs de vitesse
-def v(t):
-    if t <= 5:
-        return 250
-    elif t <= 10:
-        return 250 - 10 * (t - 5)
-    elif t <= 15:
-        return 200 + 10 * (t - 10)
-    elif t <= 50/3:
-        return 250 - 150 * (t - 15)
-    else:
-        return 0
-
-# Vectorisation
-v_vec = np.vectorize(v)
-v = v_vec(t)
+import numpy as np
+x0=0
 
 
-# Tracé de la vitesse
-plt.plot(t, v, label="v(t) : vitesse de la F1", color="red", linewidth=2)
-plt.axvline(5, linestyle="--", color="gray", alpha=0.5, label="Début virage 1")
-plt.axvline(10, linestyle="--", color="gray", alpha=0.5)
-plt.axvline(15, linestyle="--", color="black", alpha=0.5, label="Crash")
-plt.title("Vitesse d'une Formule 1 sur Spa-Francorchamps")
-plt.xlabel("Temps (s)")
-plt.ylabel("Vitesse (km/h)")
+# Charger le fichier CSV
+data = pd.read_csv("vitesse1.csv")  # Remplace par le nom réel du fichier
+temps = np.array(data["Time (s)"])
+vitesse_m_s = np.array(data["Speed (m/s)"])
+vitesse_km_h = 3.6*vitesse_m_s #conversion en km/h pour le tracé de vitesse
+
+
+#calcul de l'accélération (dérivée (euler progressive))
+acc=np.zeros(len(vitesse_m_s))
+for i in range(len(temps)-1):
+    acc[i]=(vitesse_m_s[i+1]-vitesse_m_s[i])/(temps[i+1]-temps[i])
+
+
+#calcul de la position (intégrale par méthode trapèze)
+distance = np.zeros(len(temps)-1)  
+distance[0]=x0
+
+for i in range(len(temps)-1):
+    dt = temps[i+1] - temps[i]
+    dv = (vitesse_m_s[i] + vitesse_m_s[i+1]) / 2
+    dx = dv * dt
+    distance[i]=distance[i-1] + dx
+temps_pos=[]
+for i in range(len(temps)-1):
+    temps_pos.append(temps[i])
+
+
+
+#réponse aux questions
+
+#recherche pic accélération
+acc_max=0
+temps_acc_max=0
+for i in range(len(acc)):
+    if acc[i]>acc_max:
+        acc_max=acc[i]
+        temps_acc_max=temps[i]
+print('')
+print(f"la valeur d'accélération maximale est de {acc_max:.3f}m/s², on atteint cette valeur à {temps_acc_max:.3f}s")
+
+
+#recherche distance parcourue totale
+dist=distance[-1]
+temps_final=temps[-1]
+print("")
+print(f"La distance totale qu'on a parcourue en {temps_final:.3f}s est de {dist:.3f}m")
+
+# Tracés
+
+#vitesse
+plt.plot(temps, vitesse_km_h,label='vitesse(km/h)')
+plt.xlabel("temps(s)")
+plt.ylabel("vitesse(km/h)")
+plt.title("Tracé de la vitesse en fonction du temps")
 plt.grid(True)
 plt.legend()
+plt.show()
 
-#définition de l'accéleration exacte
-def a(t):
-    if t <= 5:
-        return 0
-    elif t <= 10:
-        return -10
-    elif t <= 15:
-        return 10
-    elif t <= 50/3:
-        return -150
-    else:
-        return 0
-    
-a_vec=np.vectorize(a)
-a=a_vec(t)
+#accélération
+plt.plot(temps, acc,label='accélération(m/s²)')
+plt.grid()
+plt.title("tracé de l'accélération en fonction du temps")
+plt.xlabel("temps(s)")
+plt.ylabel(r"accélération (m/s$^2$)")
+plt.scatter(temps_acc_max,acc_max,color='red',marker='+',zorder=10,s=100,label='accélération max')
+plt.legend()
+plt.show()
 
-plt.plot(t,a)
+#position
+plt.plot(temps_pos,distance,label='distance parcourue (m)')
+plt.grid()
+plt.title("tracé de la position en fonction du temps")
+plt.xlabel('temps(s)')
+plt.ylabel('position(m)')
+plt.legend()
 plt.show()

b40187b78c818fadb595ffef33a6c0ef80c8d910
Roxane <roxane.coulon-parame@esiroi.re>
2025-05-28 14:15:09 +0400

    grille plus précise

--- script.py
+++ script.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 x0=0
 
 
 # Charger le fichier CSV
-data = pd.read_csv("vitesse1.csv")  # Remplace par le nom réel du fichier
+data = pd.read_csv("vitesse1.csv")  
 temps = np.array(data["Time (s)"])
 vitesse_m_s = np.array(data["Speed (m/s)"])
 vitesse_km_h = 3.6*vitesse_m_s #conversion en km/h pour le tracé de vitesse
@@ -33,8 +34,10 @@ for i in range(len(temps)-1):
     temps_pos.append(temps[i])
 
 
+#==================================================================================================
+# REPONSES AUX QUESTIONS
+#==================================================================================================
 
-#réponse aux questions
 
 #recherche pic accélération
 acc_max=0
@@ -53,18 +56,39 @@ temps_final=temps[-1]
 print("")
 print(f"La distance totale qu'on a parcourue en {temps_final:.3f}s est de {dist:.3f}m")
 
-# Tracés
 
-#vitesse
+
+#==================================================================================================
+# Tracés
+#==================================================================================================
+
+
+# VITESSE
 plt.plot(temps, vitesse_km_h,label='vitesse(km/h)')
 plt.xlabel("temps(s)")
 plt.ylabel("vitesse(km/h)")
 plt.title("Tracé de la vitesse en fonction du temps")
+# ticks visibles
+plt.xticks(np.arange(0, 51, 1))
+plt.yticks(np.arange(0, 61, 1))
+# grille principale toutes les 5
+plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(10))
+plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(10))
+# grille secondaire toutes les 1
+plt.gca().xaxis.set_minor_locator(ticker.MultipleLocator(1))
+plt.gca().yaxis.set_minor_locator(ticker.MultipleLocator(1))
+# tracé des grilles
+plt.grid(which='major', linewidth=1, color='black')
+plt.grid(which='minor', linestyle='--', linewidth=0.5, color='gray')
+
+plt.tight_layout()
 plt.grid(True)
 plt.legend()
 plt.show()
 
-#accélération
+
+
+#ACCELERATION
 plt.plot(temps, acc,label='accélération(m/s²)')
 plt.grid()
 plt.title("tracé de l'accélération en fonction du temps")
@@ -74,7 +98,10 @@ plt.scatter(temps_acc_max,acc_max,color='red',marker='+',zorder=10,s=100,label='
 plt.legend()
 plt.show()
 
-#position
+
+
+
+#POSITION
 plt.plot(temps_pos,distance,label='distance parcourue (m)')
 plt.grid()
 plt.title("tracé de la position en fonction du temps")

659f685c6c8ac1655eabc9ba8a2472ce7e108d9c
974Rider <massonalexandre8@gmail.com>
2025-05-28 11:06:54 +0400

    légalisation des données

---
+++ script_brouillon.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+v0=0
+x0=0
+
+# Charger le fichier CSV
+data = pd.read_csv("vitesse1.csv")  # Remplace par le nom réel du fichier
+
+#tracé des vitesses
+temps = np.array(data["Time (s)"])
+vitesse_m_s = np.array(data["Speed (m/s)"])
+vitesse_km_h = 3.6*vitesse_m_s
+
+
+#calcul de l'accélération (dérivée)
+acc=np.zeros(len(vitesse_m_s))
+for i in range(len(temps)-1):
+    acc[i]=(vitesse_m_s[i+1]-vitesse_m_s[i])/temps[i+1]
+
+
+
+# Tracer
+plt.plot(temps, vitesse_km_h)
+plt.xlabel("colonne_x")
+plt.ylabel("colonne_y")
+plt.title("Tracé de colonne_y en fonction de colonne_x")
+plt.grid(True)
+plt.show()
